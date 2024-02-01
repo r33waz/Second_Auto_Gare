22ceaf482dbba62173bfff701a2f76d435bb3579
Riwaz Thapa <reewazthapa22@gmail.com>
2024-02-02 00:08:31 +0545

    search by model,color,displacement and transmission api

--- backend/src/routes/vehicle.routes.js
+++ backend/src/routes/vehicle.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { Addvehicle, deleteVechile, getAllVehicle, getVehicleById, updateVehicle } from "../controller/vehicle.controller.js"
+import { Addvehicle, deleteVechile, findDisplacementVehicles,findTransmissionVehicles, getAllVehicle, getColorVehicles, getVehicleById, searchByModel, updateVehicle } from "../controller/vehicle.controller.js"
 
 const router = express.Router()
 router.post("/add_vehicle", Addvehicle)
@@ -7,5 +7,9 @@ router.get("/get_allvehicles", getAllVehicle)
 router.get("/get_vehicle/:id", getVehicleById)
 router.delete("/delete_vehicle/:id", deleteVechile)
 router.patch("/update_vehicle/:id", updateVehicle)
+router.get("/vehicle", searchByModel)
+router.get("/vehicle/color/", getColorVehicles);
+router.get("/vehicle/displacement/", findDisplacementVehicles)
+router.get("/vehicle/transmission/", findTransmissionVehicles)
 
 export default router
