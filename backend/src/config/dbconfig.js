5a7600c5e425f958a85b512f3730c59a669ed744
riwazthapa <102170949+r33waz@users.noreply.github.com>
2024-05-18 22:51:36 +0545

    fixed the error in backend end from database

--- backend/src/config/dbconfig.js
+++ backend/src/config/dbconfig.js
@@ -1,11 +1,10 @@
 import mongoose from "mongoose";
-import "dotenv/config.js"
 
-const MONGO_URL = process.env.MONGO_URL;
+
 
 export const Dbconnect = async () => {
   try {
-    const database = await mongoose.connect(MONGO_URL, {
+    const database = await mongoose.connect(process.env.MONGOURL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
