efce3176cc9a2a83009b5323250eaaa7dfa667aa
riwazthapa <102170949+r33waz@users.noreply.github.com>
2024-06-17 22:32:18 +0545

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: process.env.BASE_URL,
+    origin:[""],
     credentials: true,
   })
 );
