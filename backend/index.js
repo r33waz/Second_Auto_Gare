bf70bee965634e2293614a859465175c58dcf85b
riwazthapa <102170949+r33waz@users.noreply.github.com>
2024-06-18 00:07:24 +0545

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin:["https://second-auto-gare.vercel.app/"],
+    origin:"https://second-auto-gare.vercel.app/",
     credentials: true,
   })
 );
